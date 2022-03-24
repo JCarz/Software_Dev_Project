e38fc6f4e9c4ddeb58de9ecc4074787bb12ad247
Brian Dinh <me@miito.dev>
2022-03-24 03:26:22 -0400

    Refactor ChakraSign component

--- components/chakra-sign.js
+++ components/chakra-sign.js
@@ -1,83 +1,114 @@
 import { Node, mergeAttributes } from "@tiptap/core"
-import React, { useRef, useState } from "react"
+import React, { useRef, useState, useEffect } from "react"
 import {
   ReactNodeViewRenderer,
   NodeViewWrapper,
-  NodeViewContent,
 } from "@tiptap/react"
 import {
-  ChakraProvider,
   FormControl,
   Input,
   Modal,
   ModalOverlay,
   ModalBody,
   ModalContent,
-  ModalHeader,
   useDisclosure,
   FormLabel,
   Progress,
   Button,
   ModalFooter,
   ButtonGroup,
+  Box
 } from "@chakra-ui/react"
 import SignaturePad from "react-signature-canvas"
 
-function ChakraSign() {
+function ChakraSign(props) {
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
-  const initialRef = React.useRef() //useRef returns a mutable(liable to change) ref object whose .current property is initialized to the passed argument (initialValue).Reference of the componet that has focus on when the modal opens
-  const finalRef = React.useRef() //receives focus when the modal closes.
-  const [imageURL, setImageURL] = useState(null) // create a state that will contain the url
 
-  const signatureCanvas = useRef({})
-  /**
-   * @return a funtion that uses the canvas ref to clear the canvas via a method given by react-signature-canvas
-   */
-  const clear = () => signatureCanvas.current.clear()
-  /**
-   * @returns a function that uses the canvas ref to trim the canvas from white space via a method given by react-signature-canvas and
-   * saves it in our state
-   */
-  const save = () =>
-    console.log(
-      signatureCanvas.current.getTrimmedCanvas().toDataURL("image/png")
-    )
+  const [firstName, setFirstName] = useState("");
+  const [title, setTitle] = useState("");
+  const [company, setCompany] = useState("");
+
+  const [signatureData, setSignatureData] = useState([]);
+  const [signatureImg, setSignatureImg] = useState(null);
+  const [signatureEnd, setSignatureEnd] = useState(false);
+
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    canvasRef.current?.fromData(signatureData);
+  }, [signatureData])
+
+  const modalClose = () => {
+
+  }
+
+  const modalOpen = () => {
+    onOpen();
+  }
+
+  const Delete = () => {
+    props.deleteNode()
+    onClose();
+  }
+
+  const noEmptyField = !canvasRef.current?.isEmpty() && firstName !== "" && title !== "";
+
+  const Save = () => {
+    if (noEmptyField) {
+      setSignatureData(canvasRef.current?.toData());
+      setSignatureImg(canvasRef.current?.getTrimmedCanvas().toDataURL("image/png"));
+      onClose();
+    }
+  }
+
+  const Clear = () => {
+    setSignatureData([]);
+    setSignatureImg(null);
+    onClose();
+  }
 
   return (
     <NodeViewWrapper className="chakra-sign">
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalBody>
-            <FormControl>
-              <FormLabel>First Name</FormLabel>
-              <Input placeholder="First name" />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Title</FormLabel>
-              <Input placeholder="Type Here" />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Company(optional)</FormLabel>
-              <Input placeholder="Type Here" />
-            </FormControl>
-            <SignaturePad/>
-            <p>
-              <Progress size="xs" isIndeterminate hasStripe />
-              Waiting for <strong>Persons</strong> signature
-            </p>
-          </ModalBody>
-          <ModalFooter justifyContent="space-between">
-            <ButtonGroup>
-              <Button colorScheme="red" onClick={Delete}>Delete</Button>
-            </ButtonGroup>
-            <ButtonGroup>
-              <Button colorScheme="green" onClick={Save}>Save</Button>
-              <Button onClick={Clear}>Clear</Button>
-            </ButtonGroup>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <div>
+        <Box p="2" border="1px" onClick={modalOpen}>
+          { signatureImg ? <img src={signatureImg} alt="signature" /> : <p>Sign Here</p> }
+        </Box>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalBody>
+              <FormControl>
+                <FormLabel>First Name</FormLabel>
+                <Input placeholder="First name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Title</FormLabel>
+                <Input placeholder="Type Here" value={title} onChange={(e) => setTitle(e.target.value)}/>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Company(optional)</FormLabel>
+                <Input placeholder="Type Here" value={company} onChange={(e) => setCompany(e.target.value)}/>
+              </FormControl>
+              <SignaturePad ref={canvasRef} onEnd={() => setSignatureEnd(true)}/>
+              {!signatureEnd && (
+                <p>
+                  <Progress size="xs" isIndeterminate hasStripe />
+                  Waiting for <strong>Persons</strong> signature
+                </p>
+              )}
+            </ModalBody>
+            <ModalFooter justifyContent="space-between">
+              <ButtonGroup>
+                <Button colorScheme="red" onClick={Delete}>Delete</Button>
+              </ButtonGroup>
+              <ButtonGroup>
+                <Button colorScheme="green" onClick={Save}>Save</Button>
+                <Button onClick={Clear}>Clear</Button>
+              </ButtonGroup>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </div>
     </NodeViewWrapper>
   )
 }
@@ -87,7 +118,7 @@ export default Node.create({
 
   group: "block",
 
-  content: "inline*",
+  content: "block*",
 
   parseHTML() {
     return [
